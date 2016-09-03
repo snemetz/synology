2aed91656c2523b167401fa4bc53a8b2b86aa2e3
Steven Nemetz <snemetz@hotmail.com>
2016-09-03 09:22:31 -0700

    Cleanup

--- HDHomeRun/firmware-dl.sh
+++ HDHomeRun/firmware-dl.sh
@@ -8,39 +8,18 @@
 #   hdhomerun_config
 #   jq  https://stedolan.github.io/jq/
 #
-# http://download.silicondust.com/hdhomerun/hdhomerun3_cablecard_firmware_20160630atest2.bin
-#	20160901beta3
 #TODO:
 #  How to determine latest version to download
-#  Get firmware version on HDHomeRun Prime
-#  Upgrade HDHomeRun if got newer version and HDHomeRun is idle
-#  Upgrade all HDHomeRun devices
 #  Notify user of upgrade
 #
-#Notes:
-#  Via Web
-#    Get devices and IPs in json:
-#      http://my.hdhomerun.com/devices
-#      Fields: device_id, local_ip
-#    Get firmware version
-#      http://<local ip>/discover.json
-#      get fields: FirmwareName, FirmwareVersion
-#      Filename: ${FirmwareName}_firmware_${FirmwareVersion}.bin
-#  Via CLI: hdhomerun_config
-#    Get devices:
-#      hdhomerun_config discover
-#    Get firmware version:
-#      hdhomerun_config <device ID> get /sys/version
-#    Check if device is idle
-#    Upgrade firmware:
-#      hdhomerun_config <device ID> upgrade <firmware file>
 
 FirmwareCurrent=20160902beta3
 # Synology with VideoStation installed
 hdhomerun_config=/volume1/@appstore/VideoStation/bin/hdhomerun_config
+# Path to archive directory of firmware files
 DirArchive=/volume1/software/Apps-Installed/HTPC/HDHomeRun/Firmware
 DirTmp=/tmp
-Log=hdhomerun_dl.log
+Log=${DirTmp}/hdhomerun_firmware_dl.log
 DL_URLBase=http://download.silicondust.com/hdhomerun
 FileDevicesDiscover=${DirTmp}/discover_devices.json
 FileDeviceDiscover=${DirTmp}/discover_device.json
